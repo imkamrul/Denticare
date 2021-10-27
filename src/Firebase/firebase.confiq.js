75a276d5f635a6d2e6f2f28d6fef60487eabe886
k17h02 <k17h02@gmail.com>
2021-10-27 17:46:18 +0600

    firebase

--- src/Firebase/firebase.confiq.js
+++ src/Firebase/firebase.confiq.js
@@ -1,16 +1,11 @@
 console.log(process.env)
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FIREABSE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREABSE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREABSE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREABSE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREABSE_APP_ID,
-    apiKey: "AIzaSyATCTCgJslJkIifYk7cPUE99jtK_FQmcPY",
-    authDomain: "denticare-k17h02.firebaseapp.com",
-    projectId: "denticare-k17h02",
-    storageBucket: "denticare-k17h02.appspot.com",
-    messagingSenderId: "779570191582",
-    appId: "1:779570191582:web:f7f8dba1e17ce1485e364f"
+    apiKey: process.env.REACT_APP_FIREABSE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREABSE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREABSE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREABSE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREABSE_APP_ID,
+
 };
 export default firebaseConfig;
